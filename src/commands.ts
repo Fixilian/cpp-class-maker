aad79f91b2df35be3964bb5723371f0f1d5b5e88
Ilya Dyakun <i.dyakun@g.nsu.ru>
2025-10-07 20:19:25 +0700

    Implement basic extension structure

---
+++ src/commands.ts
@@ -0,0 +1,36 @@
+import * as vscode from 'vscode';
+
+export type Callback = (args: any[]) => any;
+
+export const commandRegistry = new Map<string, Callback>([
+    ['cpp-class-maker.initProject', initProjectCommand], 
+    ['cpp-class-maker.newClass', newClassCommand], 
+    ['cpp-class-maker.newClassNoTest', newClassNoTestCommand], 
+    ['cpp-class-maker.newHeader', newHeaderCommand], 
+    ['cpp-class-maker.newTemplateClass', newTemplateClassCommand], 
+    ['cpp-class-maker.newTemplateClassNoTest', newTemplateClassNoTestCommand], 
+]);
+
+function initProjectCommand () { 
+    vscode.window.showInformationMessage('Init Project!');
+};
+
+function newClassCommand () { 
+    vscode.window.showInformationMessage('New Class!');
+};
+
+function newClassNoTestCommand () { 
+    vscode.window.showInformationMessage('New Class w/o test!');
+};
+
+function newHeaderCommand () { 
+    vscode.window.showInformationMessage('New Header!');
+};
+
+function newTemplateClassCommand () { 
+    vscode.window.showInformationMessage('New Template Class!');
+};
+
+function newTemplateClassNoTestCommand () { 
+    vscode.window.showInformationMessage('New Template Class w/o test!');
+};
