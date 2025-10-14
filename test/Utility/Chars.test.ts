e0844823563d524fcb53094ec4df98df380507bd
Ilya Dyakun <i.dyakun@g.nsu.ru>
2025-10-14 09:54:16 +0700

    Implement char code functions

--- test/Utility/Chars.test.ts
+++ test/Utility/Chars.test.ts
@@ -1,3 +1,4 @@
+import * as assert from 'assert';
 import * as chars from '../../src/Utility/Chars';
 import * as tester from '../TestUtility/Tester';
 
@@ -19,4 +20,54 @@ suite("Chars Module Test Suite", () => {
         ];
         tester.runTestCases(chars.isAlpha, testCases);
     });
+
+    test("isWhitespace", () => {
+        const testCases: [string, boolean][] = [
+            [" ", true],
+            ["\n", true],
+            ["\r", true],
+            ["\t", true],
+            ["B", false],
+            ["Y", false],
+            ["b", false],
+            ["y", false],
+            ["0", false],
+            ["1", false],
+            ["8", false],
+            ["_", false],
+            ["-", false],
+            ["", false],
+        ];
+        tester.runTestCases(chars.isWhitespace, testCases);
+    });
+
+    test("isUpperLetter", () => {
+        const begin = 'A'.charCodeAt(0);
+        const end = 'Z'.charCodeAt(0);
+        testCharRange(begin, end, chars.isUpperLetter);
+    });
+
+    test("isLowerLetter", () => {
+        const begin = 'a'.charCodeAt(0);
+        const end = 'z'.charCodeAt(0);
+        testCharRange(begin, end, chars.isLowerLetter);
+    });
+
+    test("isDigit", () => {
+        const begin = '0'.charCodeAt(0);
+        const end = '9'.charCodeAt(0);
+        testCharRange(begin, end, chars.isDigit);
+    });
 });
+
+function testCharRange(begin: number, end: number, func: (ch: number) => boolean) {
+    for (let i = 0; i < begin; i += 1) {
+        assert.equal(func(i), false);
+    }
+    for (let i = begin; i <= end; i += 1) {
+        assert.equal(func(i), true);
+    }
+    for (let i = end + 1; i < 128; i += 1) {
+        assert.equal(func(i), false);
+    }
+}
