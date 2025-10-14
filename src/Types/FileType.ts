6b2fbbabf3f93008868fdc2875c52adf893a15f8
Ilya Dyakun <i.dyakun@g.nsu.ru>
2025-10-14 14:52:53 +0700

    Add file type check

--- src/Types/FileType.ts
+++ src/Types/FileType.ts
@@ -13,3 +13,20 @@ export const FileTypes = {
  * FileType provides type for FileTypes enum.
  */
 export type FileType = (typeof FileTypes)[keyof typeof FileTypes];
+
+/**
+ * Checks if file type is Test or Source.
+ * 
+ * @param type type to check. 
+ * @returns true if type is Test or Source.
+ */
+export function isSourceOrTest(type: FileType): boolean {
+    switch (type) {
+    case FileTypes.Source: return true;
+    case FileTypes.Test: return true;
+    case FileTypes.Header: // fall through 
+    case FileTypes.EmptyHeader: // fall through 
+    case FileTypes.TemplateClassHeader: return false;
+    default: throw new Error(`Unknown file type ${type}`);
+    }
+}
